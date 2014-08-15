b8ba3706bc67d257fd22f7953d361930f51f1f06
Daniel Patton <daniel.t.patton@gmail.com>
2014-08-15 14:45:20 +1000

    Added personal keybindings

---
+++ lisp/init-keybinds.el
@@ -0,0 +1,31 @@
+(define-key isearch-mode-map "\C-h" 'isearch-delete-char)
+;; TODO: make M-h delete-word in isearch
+
+;; Make C-h and M-h backspace keys
+(defvar my-overriding-binding-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [?\C-h] 'delete-backward-char)
+    (define-key map [?\M-h] 'backward-kill-word)
+        (define-key map [?\M-o] 'other-window)
+    map))
+(define-minor-mode my-overriding-binding-mode
+  "Personal global key-bindings."
+  :global t)
+(my-overriding-binding-mode 1)
+
+(setq help-char [f1])                           ; I don't want help when I'm just trying to backspace!
+(global-unset-key [f2])							; I'll set F2 myself thanks
+(global-unset-key [f10])						; No F10 menu key
+(global-unset-key (kbd "C-z"))                  ; Stop that pesky accidental hide
+(global-set-key (kbd "M-?") 'mark-paragraph)	; Use this more!
+(global-set-key (kbd "<f5>") 'recompile)		; Also this
+(global-set-key (kbd "M-o") 'other-window)		; Handy key for other-window
+
+;;; SLIME mode
+;; (eval-after-load 'slime
+;; `(define-key slime-prefix-map [f2] 'slime-documentation-lookup)) ;; BROKEN!
+
+;; Fullscreen
+;; (global-set-key (kbd "<f11>") 'toggle-fullscreen)     ; BROKEN!
+
+(provide 'init-keybinds)
