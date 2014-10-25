5bb3881d4fbfcadc443362a0a9e32ffb59e2ac4b
Daniel Patton <dnathief@gmail.com>
2014-10-25 21:38:18 +1100

    Removed unused M-h binding

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -10,7 +10,6 @@
     (dolist (direction '("right" "left"))
       (global-set-key (kbd (concat "<" multiple "wheel-" direction ">")) 'ignore)))
   (global-set-key (kbd "M-`") 'ns-next-frame)
-  (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
   (global-set-key (kbd "M-˙") 'ns-do-hide-others)
   (after-load 'nxml-mode
     (define-key nxml-mode-map (kbd "M-h") nil))
